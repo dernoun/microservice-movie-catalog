03c59f9b12c388e62b85bdc137d7c54035a28db7
dernoun <med.dernoun@gmail.com>
2022-01-05 09:42:12 -0500

    Create the catalog

---
+++ movie-catalog-service/src/main/java/dz/itbridge/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package dz.itbridge.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dz.itbridge.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		return Collections.singletonList(
+				new CatalogItem("Transformer", "Car and truck to human", 4));
+	}
+
+}

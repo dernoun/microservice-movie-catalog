a310491b9aabfbbd5766ae0e76c5f161313b5547
dernoun <med.dernoun@gmail.com>
2022-01-04 20:13:50 -0500

    create spring boot projects

---
+++ movie-catalog-service/src/main/java/dz/itbridge/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package dz.itbridge.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ movie-info-service/src/main/java/dz/itbridge/movieinfoservice/MovieInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package dz.itbridge.movieinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiceApplication.class, args);
+	}
+
+}

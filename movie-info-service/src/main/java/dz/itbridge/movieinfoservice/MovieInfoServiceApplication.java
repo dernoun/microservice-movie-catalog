f3349ec87092f5f9bd2d5b3f0f17ac670cad9980
dernoun <med.dernoun@gmail.com>
2022-01-06 14:55:47 -0500

    Add Eureka Client

--- movie-info-service/src/main/java/dz/itbridge/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/dz/itbridge/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,7 +3,7 @@ package dz.itbridge.movieinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {

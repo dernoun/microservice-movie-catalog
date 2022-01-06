747a5f915873a925aee70580c19f0d71763cbca4
dernoun <med.dernoun@gmail.com>
2022-01-06 13:40:30 -0500

    Create Eureka Discovery Server

---
+++ discovery-server/src/test/java/dz/itbridge/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.itbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

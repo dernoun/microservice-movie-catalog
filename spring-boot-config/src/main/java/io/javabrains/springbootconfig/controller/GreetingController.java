c1eec8e794f8d37840754c96019a5058b18b44db
dernoun <med.dernoun@gmail.com>
2022-02-05 17:32:20 -0500

    Add Value and list and SPEL expresion

--- spring-boot-config/src/main/java/io/javabrains/springbootconfig/controller/GreetingController.java
+++ spring-boot-config/src/main/java/io/javabrains/springbootconfig/controller/GreetingController.java
@@ -1,5 +1,8 @@
 package io.javabrains.springbootconfig.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,15 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting: default value}")
 	private String greetingMessage;
 	
-	@Value("${app.description}")
+	@Value("my static text")
 	private String appInfo;
+	
+	@Value("${my.list.values}")
+	private List<String> listValues;
+	
+	@Value("#{${app.dbValues}}")
+	private Map<String, String> dbValues;
 	
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return greetingMessage + appInfo + listValues + dbValues;
 	}
 	
 	@GetMapping("/app")
